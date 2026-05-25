8e6c4542404d3ad8d97b893665434943a005c681
Lev <prilepin1708@gmail.com>
2026-05-25 23:09:06 +0300

    Добавлена поддержка PWA и офлайн-режима

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'kalendarius-v1';
+const ASSETS = [
+    '/',
+    '/static/style.css',
+    '/static/script.js',
+    '/static/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
